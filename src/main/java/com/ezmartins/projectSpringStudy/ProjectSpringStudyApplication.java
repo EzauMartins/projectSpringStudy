6be538f4d2f6860b0cd78d9602c2c277b6832244
Ezau Martins <ezaumateus97@gmail.com>
2021-09-06 20:35:36 -0400

    Initial commit

---
+++ src/main/java/com/ezmartins/projectSpringStudy/ProjectSpringStudyApplication.java
@@ -0,0 +1,13 @@
+package com.ezmartins.projectSpringStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ezmartins/projectSpringStudy/ProjectSpringStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezmartins.projectSpringStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
